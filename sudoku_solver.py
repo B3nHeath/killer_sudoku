d765b706741f0ed1272d21396db5995025e6e41c
Ben Heath <b3n.heath@hotmail.com>
2024-07-16 14:55:22 +0100

    Added a solve function, and made stylistic changes to sudoku solver

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,7 +1,3 @@
-## I am going to start by creating a standard sudoku solver, and then I can move onto the killer sudoku. We will start by transferring a lot of the other problem over. 
-
-from itertools import combinations
-import itertools
 import math
 
 class Puzzle:
@@ -10,57 +6,75 @@ class Puzzle:
             # Create the matrix, with all possible values for all possible rows and columns
             self.matrix = [[[x for x in range(1, 10)]
                              for _ in range(9)] for _ in range(9)]
-            # Generate dictionary of boxes and their cells
+            # Generate dictionary of 3x3 boxes and their respective cells
             self.boxes = self.generate_boxes()
 
 
-    def generate_boxes(self):
-        boxes = {}
-        box_id = 0
-        # Loop over each 3x3 box starting point
-        for box_row in range(0, 9, 3):
-            for box_col in range(0, 9, 3):
-                box_cells = []
-                # Loop over each cell within a 3x3 box
-                for row in range(3):
-                    for col in range(3):
-                        box_cells.append((box_row + row, box_col + col))
-                # Add the cells to the dictionary
-                boxes[f"box_{box_id}"] = box_cells
-                box_id += 1
+    # Prints the sudoku with all possible values for each cell. 
+    def __str__(self):
+        # The full sudoku template
+        full_matrix = ""
+        # For each row in the matrix
+        for row in self.matrix:
+            # Create template, which we will use to replace values
+            template = "| X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X |\n| X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X |\n| X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X |\n"
+            # Turn it into a list so it is mutable
+            template = list(template)
+            # Print seprator for each new row
+            full_matrix += "-"*73 + "\n"
+            # for each cell in row
+            for index,cell in enumerate(row):
+                # If it is an int, place it in the top left X
+                if isinstance(cell, int):
+                    position = 2 + (8 * index)
+                    template[position] = str(cell)
+                else:
+                    # For each number in the cell
+                    for num_pos, num in enumerate(cell):
+                        # Calculate its position in the above template
+                        position = 2 + (2 * (num_pos % 3)) + (8 * index) + (74 * (num_pos // 3))
+                        # Replace position in template with number
+                        template[position] = str(num)
+            # return template to string format
+            template = "".join(template)
+            # Append it to our full template
+            full_matrix += template
+        # Once all rows are added, add a final horizontal seperator
+        full_matrix += "-"*73 + "\n"
+        return full_matrix
 
-        return boxes
+
+    # A function to solve the sudoku
+    def solve(self):
+        # If the puzzle is not solved
+        while not self.is_solved():
+            # Store the original
+            old_matrix = str(self.matrix)
+            # Check if any cells have been solved
+            self.check_solved()
+            # Remove possibilities based on known values in...
+            # Rows
+            self.check_row_singles()
+            # Columns
+            self.check_col_singles()
+            # Boxes
+            self.check_box_singles()
+            # If no progress has been made
+            if str(self.matrix) == old_matrix:
+                print("No further progress can be made with current techniques.")
+                break
 
 
+    # Check whether the sudoku is solved
+    def is_solved(self):
+        # If there are any lists in our cells, it is not solved
+        for row in self.matrix:
+            for cell in row:
+                if isinstance(cell, list):
+                    return False
+        # If you don't find any, it is solved
+        return True
 
-    # Pretty printing of the sudoku, so that we can see the possible values for each square printed in a clearer format
-    def pretty_print(self):
-            # For each row in the matrix
-            for row in self.matrix:
-                # Create template, which we will use to replace values
-                template = "| X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X |\n| X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X |\n| X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X | X X X |\n"
-                # Turn it into a list so it is mutable
-                template = list(template)
-                # Print seprator for each new row
-                print("-"*73) 
-                # for each cell in row
-                for index,cell in enumerate(row):
-                    if isinstance(cell, int):
-                        position = 2 + (8 * index)
-                        template[position] = str(cell)
-                    else:
-                        # For each number in the cell
-                        for num_pos, num in enumerate(cell):
-                            # Calculate its position in the above template
-                            position = 2 + (2 * (num_pos % 3)) + (8 * index) + (74 * (num_pos // 3))
-                            # Replace position in template with number
-                            template[position] = str(num)
-                # return template to string
-                template = "".join(template)
-                # Print
-                print(template, end = "")
-            # Print final separator
-            print("-"*73)
 
     # A function to find instances in the sudoku where there is only one possible number, and to replace it with an int
     def check_solved(self):
@@ -73,7 +87,8 @@ class Puzzle:
                     # Replace the cell with an int of the number
                     self.matrix[row_pos][col_pos] = cell[0]
                         
-    # A program that checks for single values in rows, and removes them from other cells in the same row
+
+    # A program that checks for known values in rows, and removes them from other cells in the same row
     def check_row_singles(self):
         # For each row
         for y in range(9):
@@ -92,29 +107,31 @@ class Puzzle:
                     # Remove any impossibilities (based on singles) from the list
                     self.matrix[y][x] = [item for item in self.matrix[y][x] if item not in singles]
 
-    # A function that checks for single values in columns, and removes them from other cells in the same row
+
+    # A function that checks for known values in columns, and removes them from other cells in the same column
     def check_col_singles(self):
-        # for each column
+        # For each column
         for x in range(9):
             # Create an empty cell list
             cells = []
-            # For each cell in column
+            # For each cell in the column
             for y in range(9):
                 cells.append(self.matrix[y][x])
-            # Isolate any single valyes
+            # Isolate any single values
             singles = self.check_singles(cells)
             # For each cell in the column
             for y in range(9):
                 if isinstance(self.matrix[y][x], list):
                     self.matrix[y][x] = [item for item in self.matrix[y][x] if item not in singles]
 
+
     # A function that checks single values in boxes, and removes them from other cells in the same row
     def check_box_singles(self):
         # For each of the boxes, extract the relevant cells
         for box in self.boxes.values():
-            # create an empty cell list
+            # Create an empty cell list
             cells = []
-            # Extract position for each relevant cell
+            # Extract position of each relevant cell
             for y, x in box:
                 cells.append(self.matrix[y][x])
             # Isolate any single values
@@ -138,28 +155,44 @@ class Puzzle:
         # Return known values
         return singles
 
-        
+    # Generates a dictionary of the 9 3x3 boxes that make a sudoku grid
+    def generate_boxes(self):
+        boxes = {}
+        box_id = 0
+        # Loop over each 3x3 box starting point
+        for box_row in range(0, 9, 3):
+            for box_col in range(0, 9, 3):
+                box_cells = []
+                # Loop over each cell within a 3x3 box
+                for row in range(3):
+                    for col in range(3):
+                        box_cells.append((box_row + row, box_col + col))
+                # Add the cells to the dictionary
+                boxes[f"box_{box_id}"] = box_cells
+                box_id += 1
+        return boxes
+
+
+
 def main():
     puzzle = Puzzle()
     # Transferring over puzzle contents
-    # Always add an extra 0 at the start to even out the numbers
     numbers = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
 
+    # replace known values in the sudoku
     for index, cell in enumerate(numbers):
          if cell != "0":
               row = math.floor(index/9)
               column = index % 9
               puzzle.matrix[row][column] = int(cell)
 
-    for _ in range(10):
-        puzzle.check_solved()
-        puzzle.check_row_singles()
-        puzzle.check_solved()
-        puzzle.check_col_singles()
-        puzzle.check_solved()
-        puzzle.check_box_singles()
+    print(puzzle)
+    puzzle.solve()
+    print(puzzle)
 
-    puzzle.pretty_print()
+
+    # Got a couple of improvements to make. I need to start incorporating naked pairs calculation. This seems like it might help to sort out the more complex problems in the project Euler problem, at least it will make some progress on them. It shouldn't be too difficut. 
+    # I also need to make it either more interactive or smarter. Currently it just runs through them blindly, and I have to tell it what to do. In reality I would like to make it so that it checks whether progress was made or not, if so it consolidates this progress and repeats, otherwise it moves to more complex methods in an iterative process. 
         
 
 
